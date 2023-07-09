4fd2b308f0006443122b9eb70868b9dadba19440
Deepasubramani <deepathana1998@gmail.com>
2023-07-09 14:09:40 +0100

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+      using (SqlConnection connection = new SqlConnection(connectionString))
+      {
+        try
+        {
+          connection.Open();
+          Console.WriteLine("Connection successful!");
+          connection.Close();
+        }
+        catch (SqlException ex)
+        {
+          Console.WriteLine("Connection failed: " + ex.Message);
+        }
+      }
+    
+  }
+  }
+}
